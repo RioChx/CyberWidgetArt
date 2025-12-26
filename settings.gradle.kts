cfc00fb67be705d5c0f4c9b5c810fc30153f14f3
RioChx <73mahy@gmail.com>
2025-12-27 02:56:54 +1100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,18 @@
+pluginManagement {
+    repositories {
+        google()                  // Required for Android plugins
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        google()                  // Required for Android libraries
         mavenCentral()
     }
 }
-rootProject.name = "CyberWidget Art"
-include(":app")
+
+rootProject.name = "CyberWidgetArt"
+include(":app")                   // Change if your module is named differently (usually :app)
